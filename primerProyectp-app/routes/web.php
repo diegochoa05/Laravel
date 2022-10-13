b3a7a0db2066b8abad75a953f82edf6dc2faad60
Diego Ochoa <diegochoa0510@gmail.com>
2022-10-13 09:27:20 -0600

    Rutas

---
+++ primerProyectp-app/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hola', function () {
+    echo ('Hola');
+});
+
+Route::get('saludo/{name}', function ($name) {
+    echo ('Hola' . $name);
+});
+
+Route::get('suma/{num1}/{num2}', function($num1, $num2){
+    echo $num1 + $num2;
+})->where(['num1', '[0-9]+'], ['num2', '[0-9]+']);

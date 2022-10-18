fd03debe26c04657a3625c2922ccfd12c00716aa
Diego Ochoa <diegochoa0510@gmail.com>
2022-10-18 09:56:13 -0600

    Uso de controladores y migraciones

--- primerProyectp-app/routes/web.php
+++ primerProyectp-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -34,3 +35,11 @@ Route::get('suma/{num1}/{num2}/{num3?}', function($num1, $num2, $num3=0){
 // Route::post('suma/', function(Request $request){
 
 // });
+
+Route::get('users/', [UserController::class, 'index']);
+
+Route::get('users/create', [UserController::class, 'create']);
+
+Route::get('users/{id}', [UserController::class, 'show']);
+
+Route::post('users/', [UserController::class, 'store']);
